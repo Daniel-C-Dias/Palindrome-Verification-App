5fee70ca614cad9178a675f5b9a59d56dfe515fe
Daniel-C-Dias <danielcanastro@gmail.com>
2020-02-24 22:00:39 +0000

    Add project files.

---
+++ PalindromeVerificationApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PalindromeVerificationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
